003818b56e1dd4e3d7307cfa57d0833c5d44686a
WuYichen <2950335254@qq.com>
2022-10-14 20:39:00 +0800

    minor: modify shadow_debug.hlsl

--- ZeroRenderer/Shaders/ShadowDebug.hlsl
+++ ZeroRenderer/Shaders/ShadowDebug.hlsl
@@ -1,6 +1,15 @@
-//***************************************************************************************
-// ShadowDebug.hlsl by Frank Luna (C) 2015 All Rights Reserved.
-//***************************************************************************************
+// Defaults for number of lights.
+#ifndef NUM_DIR_LIGHTS
+    #define NUM_DIR_LIGHTS 3
+#endif
+
+#ifndef NUM_POINT_LIGHTS
+    #define NUM_POINT_LIGHTS 0
+#endif
+
+#ifndef NUM_SPOT_LIGHTS
+    #define NUM_SPOT_LIGHTS 0
+#endif
 
 // Include common HLSL code.
 #include "Common.hlsl"
@@ -8,12 +17,19 @@
 struct VertexIn
 {
 	float3 PosL    : POSITION;
+    float3 NormalL : NORMAL;
 	float2 TexC    : TEXCOORD;
+	float3 TangentU : TANGENT;
 };
 
 struct VertexOut
 {
 	float4 PosH    : SV_POSITION;
+    float4 ShadowPosH : POSITION0;
+    float4 SsaoPosH   : POSITION1;
+    float3 PosW    : POSITION2;
+    float3 NormalW : NORMAL;
+	float3 TangentW : TANGENT;
 	float2 TexC    : TEXCOORD;
 };
 
@@ -21,17 +37,39 @@ VertexOut VS(VertexIn vin)
 {
 	VertexOut vout = (VertexOut)0.0f;
 
-    // Already in homogeneous clip space.
-    vout.PosH = float4(vin.PosL, 1.0f);
-	
-	vout.TexC = vin.TexC;
-	
+	// Fetch the material data.
+	MaterialData matData = gMaterialData[gMaterialIndex];
+
+    // Transform to world space.
+    float4 posW = mul(float4(vin.PosL, 1.0f), gWorld);
+    vout.PosW = posW.xyz;
+
+    // Assumes nonuniform scaling; otherwise, need to use inverse-transpose of world matrix.
+    vout.NormalW = mul(vin.NormalL, (float3x3)gWorld);
+
+	vout.TangentW = mul(vin.TangentU, (float3x3)gWorld);
+
+    // Transform to homogeneous clip space.
+    vout.PosH = mul(posW, gViewProj);
+
+    // Generate projective tex-coords to project SSAO map onto scene.
+    vout.SsaoPosH = mul(posW, gViewProjTex);
+
+	// Output vertex attributes for interpolation across triangle.
+	float4 texC = mul(float4(vin.TexC, 0.0f, 1.0f), gTexTransform);
+	vout.TexC = mul(texC, matData.MatTransform).xy;
+
+    // Generate projective tex-coords to project shadow map onto scene.
+    vout.ShadowPosH = mul(posW, gShadowTransform);
+
     return vout;
 }
 
 float4 PS(VertexOut pin) : SV_Target
 {
-    return float4(gSsaoMap.Sample(gsamLinearWrap, pin.TexC).rrr, 1.0f);
+    // Only the first light casts a shadow.
+    float3 shadowFactor = float3(1.0f, 1.0f, 1.0f);
+    shadowFactor[0] = CalcShadowFactor(pin.ShadowPosH);
+
+    return float4(shadowFactor.rrr, 1.0f);
 }
-
-
